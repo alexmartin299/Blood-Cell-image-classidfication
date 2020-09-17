bed6eef0c40c2f53fce524f907a5ff9ac64bb6b8
alexmartin299 <68471962+alexmartin299@users.noreply.github.com>
2020-09-17 13:16:46 +0200

    Update Project_all_together.py

--- Project_all_together.py
+++ Project_all_together.py
@@ -5,87 +5,8 @@ Created on Sun May 17 16:51:36 2020
 @author: Alex Martin
 """
 
-"""CODI PER AL PROCESAMENT D'IMATGES I GUARDAT A UNA ALTRA CARPETA"""
 
-import matplotlib.pyplot as plt
-import sklearn.cluster as sk
-import glob
-import os
-import numpy as np
-"""importacio d'imatges per entrenar el model KMeans y poder fer la posterios segmentacio"""
-
-rgb = plt.imread('celula.jpeg')
-rgb2 = plt.imread('celula2.jpeg')
-m,n,l = rgb.shape
-o,p,q = rgb2.shape
-
-rgbr = np.zeros((m,n,6))
-rgbr[:,:,0] = rgb[:,:,0]
-rgbr[:,:,1] = rgb[:,:,1]
-rgbr[:,:,2] = rgb[:,:,2]
-rgbr[:,:,3] = rgb2[:,:,0]
-rgbr[:,:,4] = rgb2[:,:,1]
-rgbr[:,:,5] = rgb2[:,:,2]
-
-vectores = np.reshape(rgbr,(m*n+o*p,3))
-"""entrenament del model"""
-kmeans = sk.KMeans(n_clusters=4, random_state=0).fit(vectores)
-labels = np.reshape(kmeans.labels_,(240,320,2))
-
-def Segmentacio_100x100(img):
-    """funcio que troba el centre de massa de la imatge y retalla la imatge a 100x100 pixels 
-    amb aquest punt al centre als tres canals RGB y retorna la imatge RGB retallada"""
-    r,l,n = img.shape
-    vector_predict = np.reshape(img,(r*l,3))
-    predict = np.reshape(kmeans.predict(vector_predict),(240,320))
-    lila = np.uint8(predict==np.ones((240,320))*3)
-
-    x,y = np.meshgrid(np.linspace(0,319,320),np.linspace(0,239,240))
-    axis_x = np.int16(np.sum(x*lila)/np.sum(lila))
-    axis_y = np.int16(np.sum(y*lila)/np.sum(lila))
-    new_img = np.zeros((100,100,3))
-    for i in range(0,3):
-        imag_padded = np.pad(img[:,:,i], ((50, 50), (50, 50)), 'minimum')
-        new_img[:,:,i] = imag_padded[axis_y:axis_y+100,axis_x:axis_x+100]
-        
-    return new_img/new_img.max()
-
-
-def files_to_images_saved(folder_dir1,folder_dir2):
-    """extrau les imatges RGB d'una carpeta les segmenta i les torna a guardar amb una altre nom
-    a un altre carpeta """
-    data_path = os.path.join(folder_dir1,'*g')
-    src_fname, ext = os.path.splitext(data_path)
-    files = glob.glob(data_path)
-    train_image = np.zeros((100,100,3), dtype='float32')
-    for file in files:
-        image = plt.imread(file)
-        src_fname, ext = os.path.splitext(file)
-        train_image = Segmentacio_100x100(image)
-        save_fname = os.path.join(folder_dir2, os.path.basename(src_fname)+'.jpeg')
-        plt.imsave(save_fname,train_image)
-
-
-folders = list(('\EOSINOPHIL','\ZEUTROPHIL','\LYMPHOCYTE','\MONOCYTE'))
-"""bucles per realitzar la segmentacio en totes les imatges del dataset"""
-for i in folders:
-    """INDICAR DIRECCIO DE  LES CARPETES"""
-    """img_dir es la d'origen(Train) y img_dir_2 es on es guardaran les imatges(Train_segmented)"""
-    img_dir = r"" + i 
-    img_dir_2 = r"" + i 
-    files_to_images_saved(img_dir,img_dir_2)
-    
-for i in folders:
-    """INDICAR DIRECCIO DE  LES CARPETES"""  
-    """img_dir es la d'origen(Test) y img_dir_2 es on es guardaran les imatges(Test_segmented)"""
-    img_dir = r"" + i 
-    img_dir_2 = r"" + i 
-    files_to_images_saved(img_dir,img_dir_2)
-
-"""FINS AQUI EL PROCESSAMENT"""
-
-
-"""CODI PER A ENTRENAR EL DIFERENTS MODELS"""
+"""Code for training the differetns models """
 """S'HAN DE FER CAMBIS PER A FER L'ENTRENAMENT DE CADA MODEL """
 
 import numpy as np
@@ -106,9 +27,9 @@ from numpy.random import seed
 seed(0)
 
 
-"""aquesta funcio no s'utilitza cuan el input es RGB"""
+
 def Segmentacio_100x100_phase(img):
-    """funcio que rep una imatge rgb i retorna la fase d'aquesta amb les mateixes dimensions """
+    """function that returns the phase of the rgb image """
     r,l,n = img.shape
     
     
@@ -141,15 +62,14 @@ def files_to_images(folder_dir):
 
 
 folders = list(('\EOSINOPHIL','\ZEUTROPHIL','\LYMPHOCYTE','\MONOCYTE'))
-"""bucles para extraer las imagenes de las 4 carpetas 
-y colocarlas en arrays de train i test y tambien genera los respectivos labels"""
+
 total_length=0
 for i in folders:
-    """INDICAR DIRECCIO DE  LES CARPETA TRAIN SEGMENTED"""
+    """Indicate file path to the processed train images"""
     img_dir = r"" + i 
     train_images_1 , length = files_to_images(img_dir)
     total_length += length 
-"""arrays on es guarden les imatges y es generen les etiquetes per l'entrenament dels models"""
+
 train_images=np.empty((total_length,100,100,3),dtype='int16')
 train_labels=np.empty((total_length),dtype='uint8')
 label=0
@@ -157,7 +77,7 @@ length_0 = 0
 total_length=0
 
 for i in folders:
-    """INDICAR DIRECCIO DE  LES CARPETA TRAIN SEGMENTED"""
+    """ndicate file path to the processed train images"""
     img_dir = r"" + i 
     train_images_2 , length = files_to_images(img_dir)
     total_length += length 
@@ -166,8 +86,7 @@ for i in folders:
     
     label += 1
     length_0 = total_length
-    """en cas de voler fer classificacio de 4 tipus de celules utilitzar les dues 
-    lines d'adalt en cas de classificacio de 2 tipus utilitzar les 4 linies de sota"""
+    """4 type cell classification use the 2 lines above and for 2 type classification use de 5 lines below"""
     """
     length_0 = total_length
     if i == '\ZEUTROPHIL':
@@ -178,7 +97,7 @@ for i in folders:
     
 total_length=0
 for i in folders:
-    """INDICAR DIRECCIO DE  LES CARPETA TEST SEGMENTED"""
+    """ndicate file path to the processed test images"""
     img_dir = r"" + i 
     test_images_1 , length = files_to_images(img_dir)
     total_length += length 
@@ -190,7 +109,7 @@ length_0 = 0
 total_length=0
 
 for i in folders:
-    """INDICAR DIRECCIO DE  LES CARPETA TRAIN SEGMENTED"""
+    """indicate file path to the processed test images"""
     img_dir = r"" + i 
     test_images_2 , length = files_to_images(img_dir)
     total_length += length 
@@ -200,8 +119,7 @@ for i in folders:
     
     label += 1
     length_0 = total_length
-    """en cas de voler fer classificacio de 4 tipus de celules utilitzar les 
-    dues lines d'adalt en cas de classificacio de 2 tipus utilitzar les 4 linies de sota"""
+    """4 type cell classification use the 2 lines above and for 2 type classification use de 5 lines below"""
     """
     length_0 = total_length
     if i == '\ZEUTROPHIL':
@@ -209,7 +127,7 @@ for i in folders:
     else:
         label=label
     """
-"""significat del numeros a les etiquetes"""
+
 """labels eosinofilo=0, limfocito=1, monocito =2, neutrofilo=3"""
 """labels (eosinofilo,neutrofilo)polinuclear=0 (monocito,limfocito)mononuclear=1"""
 
@@ -251,7 +169,7 @@ datagen = ImageDataGenerator(
 """callback per a que guardi el model entrenat amb millor validation accuracy"""
 my_callbacks = [ModelCheckpoint(filepath='batch_phase_tanh_2types.h5' ,save_best_only=True,monitor='val_accuracy')]
 
-"""DESCOMENTAR EL MODEL QUE ES VULGUI UTILITZAR"""
+"""uncomment the model wanted to use"""
 
 """MODEL  CNN de Zhimin Gao et al. """
 """
